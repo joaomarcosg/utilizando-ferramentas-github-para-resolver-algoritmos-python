fcc02ffe4beb237df03fabdd36db95cbae38daf9
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-12 16:22:04 -0300

    adiciona desafios

---
+++ CODE-PYTHON/repet_text.py
@@ -0,0 +1,6 @@
+# solicita uma string e um inteiro como entrada. Depois retorna a string repetida o número de vezes informado.
+
+string = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+print((string + " ") * numero)
